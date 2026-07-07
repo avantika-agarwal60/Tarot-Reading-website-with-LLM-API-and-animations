3d568f80326fef708f43431a23175ac72bea08e1
avantika-agarwal60 <avantika.agarwal955@gmail.com>
2026-07-08 00:32:16 +0530

    Fix Cloudinary URLs - update cloud name and remove incorrect v1 versioning

--- yay-feel-good.js
+++ yay-feel-good.js
@@ -1,9 +1,9 @@
 const transitionLink = document.getElementById('inner');
 const playSFX = (audio) => audio.play().catch(() => {});
 const sunhover= new Audio();
-sunhover.src= "https://res.cloudinary.com/c-15e9540d6aa74bc6935e277de3f6a2/video/upload/v1/sounds/sunsound.wav";
+sunhover.src= "https://res.cloudinary.com/oih4u0sn/video/upload/v1783448747/sunsound_y3p3q5.wav";
 sunhover.playbackRate=1.3;
-const doorsound= new Audio("https://res.cloudinary.com/c-15e9540d6aa74bc6935e277de3f6a2/video/upload/v1/sounds/door.mp3");
+const doorsound= new Audio("https://res.cloudinary.com/oih4u0sn/video/upload/v1783448747/door_qmw1xj.mp3");
 if (transitionLink) {
     transitionLink.addEventListener('click', function(event) {
         event.preventDefault(); // Stop the browser from instantly changing pages
